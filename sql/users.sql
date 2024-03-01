8231cedac2037c008f55faa593a83f3357e48c84
Kruskalt <codigorap13@gmail.com>
2024-03-01 11:28:38 -0300

    agrego configuraciones y la base de datos,tambien la entidad auto

---
+++ sql/users.sql
@@ -0,0 +1,5 @@
+CREATE TABLE usuarios (
+    id INTEGER PRIMARY KEY,
+    nombre TEXT,
+    edad INTEGER
+);
---
+++ src/module/auto/entity/auto.js
@@ -0,0 +1,27 @@
+module.exports = class Club {
+    constructor({
+      id,
+      marca,
+      modelo,
+      año,
+      kms,
+      color,
+      aire,
+      pasajeros,
+      manual,
+      automatico,
+      
+    }) {
+      this.id = id;
+      this.marca = marca;
+      this.modelo = modelo;
+      this.año = año;
+      this.kms = kms;
+      this.color = color;
+      this.aire = aire;
+      this.pasajeros = pasajeros;
+      this.manual = manual;
+      this.automatico = automatico;
+      
+    }
+  };
