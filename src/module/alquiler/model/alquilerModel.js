2aa4d18cc6006e3ad629bea48a4311e3168e8a8b
Kruskalt <codigorap13@gmail.com>
2024-04-27 11:44:34 -0300

    alquiler pertenece a un cliente y a un auto

---
+++ src/module/alquiler/model/alquilerModel.js
@@ -0,0 +1,68 @@
+const { Sequelize, Model, DataTypes } = require("sequelize");
+module.exports = class alquilerModel extends Model {
+  /**
+   * @param {import('sequelize').Sequelize} sequelizeInstance
+   * @returns {typeof alquilerModel}
+   */
+  static setup(sequelizeInstance) {
+    alquilerModel.init(
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+          unique: true,
+        },
+        auto: {
+          type: DataTypes.INTEGER,
+        },
+        cliente: {
+          type: DataTypes.INTEGER,
+        },
+        precioUnitario: {
+          type: DataTypes.INTEGER,
+        },
+        desde: {
+          type: DataTypes.DATE,
+        },
+        hasta: {
+          type: DataTypes.DATE,
+        },
+        precioTotal: {
+          type: DataTypes.INTEGER,
+        },
+        medioDePago: {
+          type: DataTypes.STRING,
+        },
+        pagado: {
+          type: DataTypes.BOOLEAN,
+        },
+        updated_at: {
+          type: DataTypes.DATE,
+          defaultValue: Sequelize.NOW,
+        },
+        created_at: {
+          type: DataTypes.DATE,
+          defaultValue: Sequelize.NOW,
+        },
+      },
+      {
+        sequelize: sequelizeInstance,
+        modelName: "auto",
+        timestamps: false,
+      }
+    );
+    return alquilerModel;
+  }
+
+  /**
+   *
+   * @param {import('../../clientes/model/clienteModel')} ClienteModel
+   * @param {import("../../auto/model/autoModel")} AutoModel
+   */
+  static setupAssociations(ClienteModel, AutoModel) {
+    alquilerModel.belongsTo(ClienteModel, { foreignKey: "cliente_id" });
+    alquilerModel.belongsTo(AutoModel, { foreignKey: "auto_id" });
+  }
+};
